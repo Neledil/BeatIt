8c327ee83784b889bb97e3d580262443f9f09c07
Neledil <Joey.lacagnina@gmail.com>
2014-05-11 13:29:29 -0400

    first commit
    first commit

---
+++ Assets/CharacterControllerBeatEm.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerBeatEm : MonoBehaviour {
+
+	public GameObject gameobject;
+	public Animation walkLeftAnim;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.DownArrow)){
+			gameobject.transform.Translate(new Vector3(0.0f, -0.01f));
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			gameobject.transform.Translate(new Vector3(0.0f, 0.01f));
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			gameobject.transform.Translate(new Vector3(-0.01f, 0.0f));
+			walkLeftAnim.Play("playerWalkLeft2",PlayMode.StopAll);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			gameobject.transform.Translate(new Vector3(0.01f, 0.0f));
+
+		}
+
+	}
+}
